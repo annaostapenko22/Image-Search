4e125ad0e29c3c55413d40258fdc419c85dab4aa
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-01-17 22:17:00 +0200

    Updates

---
+++ precache-manifest.935e180c2cba575c3f3a49650d21955f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5730389976785996149d80983f3198",
+    "url": "/Image-Search/./index.html"
+  },
+  {
+    "revision": "5eaeddf2e1afe00c9ee6",
+    "url": "/Image-Search/./static/css/main.ad5b4aed.chunk.css"
+  },
+  {
+    "revision": "35921faf083c84704110",
+    "url": "/Image-Search/./static/js/2.af8eba87.chunk.js"
+  },
+  {
+    "revision": "6b1e30fbae570017776d1838e82dbe98",
+    "url": "/Image-Search/./static/js/2.af8eba87.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5eaeddf2e1afe00c9ee6",
+    "url": "/Image-Search/./static/js/main.fc41ef3b.chunk.js"
+  },
+  {
+    "revision": "8e32b72e667f71884c31",
+    "url": "/Image-Search/./static/js/runtime-main.b67d6a5f.js"
+  }
+]);
